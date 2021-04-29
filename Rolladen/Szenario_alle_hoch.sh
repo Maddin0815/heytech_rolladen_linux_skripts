ce989b6363c85f7127c3f83309221437ab661387
Maddin0815 <82046241+Maddin0815@users.noreply.github.com>
2021-04-29 21:33:50 +0200

    Update Szenario_alle_hoch.sh

--- Rolladen/Szenario_alle_hoch.sh
+++ Rolladen/Szenario_alle_hoch.sh
@@ -3,7 +3,7 @@
 #Die Zuordnung von Kanalnummern zu sprechenden Namen usw. wird in einer zentralen Datei vorgenommen, weil darauf mehrere Skripte zugreifen werden
 source /home/openhabian/scripts_prod/Rolladen/Rolladen_Variablen.config
 
-telnet 192.168.100.150 1002 <<EOF
+telnet 192.168.1.151 1002 <<EOF
 rhi
 
 rhb
